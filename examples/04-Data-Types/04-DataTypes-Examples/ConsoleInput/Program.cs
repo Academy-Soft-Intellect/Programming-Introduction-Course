7799b303314c01f79d67eb8beef27e7f5a2329aa
Babalev <ivaylo.babalev@gmail.com>
2015-04-29 21:26:28 +0300

    Upload examples

---
+++ examples/04-Data-Types/04-DataTypes-Examples/ConsoleInput/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter a number:");
+
+            string x = Console.ReadLine();
+
+            Console.WriteLine(x);
+        }
+    }
+}
