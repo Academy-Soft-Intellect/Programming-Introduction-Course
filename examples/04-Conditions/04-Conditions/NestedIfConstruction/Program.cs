2420971654ed4dff3b04c031ebe136e0d353aae7
Babalev <ivaylo.babalev@gmail.com>
2015-06-16 18:52:24 +0300

    Conditions Lecture + Examples

---
+++ examples/04-Conditions/04-Conditions/NestedIfConstruction/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedIfConstruction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer:");
+            int numInt = int.Parse(Console.ReadLine());
+
+            if (numInt%2 == 0)
+            {
+                if (numInt > 10)
+                {
+                    Console.WriteLine("The number is even and larger then 10!");
+                }
+                else
+                {
+                    Console.WriteLine("The number is even and smaller then 10!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("The number is odd!");
+            }
+        }
+    }
+}
---
+++ examples/04-Conditions/04-Conditions/Task/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an integer:");
+            int numInt = int.Parse(Console.ReadLine());
+            if (numInt >= 1 && numInt <= 5)
+            {
+                if (numInt % 2 == 0)
+                {
+                    Console.WriteLine("Even");
+                }
+                else
+                {
+                    Console.WriteLine("Odd");
+                }
+            }
+            else if (numInt >= 6 && numInt <= 15)
+            {
+                if (numInt % 3 == 0)
+                {
+                    Console.WriteLine("True");
+                }
+                else
+                {
+                    Console.WriteLine("False");
+                }
+            }
+            else
+            {
+                if (numInt == 0)
+                {
+                    Console.WriteLine("Zero");
+                }
+                else if (numInt > 0)
+                {
+                    Console.WriteLine("Positive");
+                }
+                else
+                {
+                    Console.WriteLine("Negative");
+                }
+            }
+
+        }
+    }
+}
