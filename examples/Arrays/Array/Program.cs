f5004b2c1fe0a0029e6e2af676d5c9584f0d4307
Babalev <ivaylo.babalev@gmail.com>
2015-06-23 20:28:58 +0300

    Examples

---
+++ examples/Arrays/Array/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] ourArr = new int[n];
+
+            for (int i = 0; i < ourArr.Length; i++)
+            {
+                ourArr[i] = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("=======================");
+
+
+            for (int i = 0; i < ourArr.Length; i++)
+            {
+                Console.WriteLine(ourArr[i]);
+            }
+        }
+    }
+}
