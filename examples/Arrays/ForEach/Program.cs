a90cd25a855d88bcbed4e4a83efab7b29c4841b5
Babalev <ivaylo.babalev@gmail.com>
2015-06-24 20:28:27 +0300

    ArraysExamples

---
+++ examples/Arrays/ForEach/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gosho = new string[] { "We", "Are", "The", "Champions" };
+
+            int[] arr = new int[4];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i + 5;
+            }
+
+            //Console.WriteLine(gosho[3]);
+            //Console.WriteLine(gosho[0]);
+            //Console.WriteLine(gosho[1]);
+            //Console.WriteLine(gosho[2]);
+            //Console.WriteLine(gosho.Length);
+
+            foreach (int tosho in arr)
+            {
+                Console.WriteLine(tosho + 1);
+            }
+        }
+    }
+}
