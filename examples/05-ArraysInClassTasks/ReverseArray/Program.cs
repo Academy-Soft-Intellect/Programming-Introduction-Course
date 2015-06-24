068ed1904cf258c9306c41aa2e9599ea54ca234f
Babalev <ivaylo.babalev@gmail.com>
2015-06-24 21:56:06 +0300

    ArraysTasks

---
+++ examples/05-ArraysInClassTasks/ReverseArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+            
+            for (int i = 0; i < array.Length/2; i++)
+            {
+                int temp = array[i];
+                array[i] = array[array.Length - 1 - i];
+                array[array.Length - 1 - i] = temp;
+            }
+
+            foreach (int element in array)
+            {
+                Console.WriteLine(element);
+            }
+        }
+    }
+}
---
+++ examples/05-ArraysInClassTasks/SwapVars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwapVars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+            int temp;
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("A=" + a);
+            Console.WriteLine("B=" + b);
+        }
+    }
+}
