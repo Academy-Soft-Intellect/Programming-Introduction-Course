ce181b6d8405beb4b3d6eeba8514e2ca1399cc41
Babalev <ivaylo.babalev@gmail.com>
2015-07-01 21:56:41 +0300

    Examples

---
+++ examples/08-MethodsTasksAndArraysHW/OurFirstMethod/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OurFirstMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Print();
+            PrintName();
+            Print();
+            PrintName();
+            Print();
+            PrintName();
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine("Hello");
+            PrintName();
+        }
+
+        public static void PrintName()
+        {
+            Console.WriteLine("Name");
+        }
+    }
+}
---
+++ examples/08-MethodsTasksAndArraysHW/SumNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 6;
+
+            Sum(a, b);
+        }
+
+        private static void Sum(int a, int b)
+        {
+            int sum = a + b;
+            Console.WriteLine(a+b);
+        }
+    }
+}
